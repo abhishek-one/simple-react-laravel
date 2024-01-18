c61c844869bf2732da8e0227ca0e07760d605fa0
Abhishek <abhishek.one@gmail.com>
2024-01-18 13:20:09 +0530

    Update

--- src/Protected.js
+++ src/Protected.js
@@ -4,20 +4,19 @@ import { useNavigate } from "react-router-dom";
 
 function Protected(props){
 
-    // let Cmp = props.testprops;
+    let Cmp = props.testprops;
     
-    // const redirect = useNavigate();
+    const redirect = useNavigate();
 
-    // useEffect(()=>{
-    //     if(!localStorage.getItem('details')){
-    //         redirect("/login");
-    //     }
-    // })
+    useEffect(()=>{
+        if(!localStorage.getItem('details')){
+            redirect("/login");
+        }
+    })
 
     return (
         <div>
-          {/* < Cmp /> */}
-          Unathorised
+            < Cmp />
         </div>
     )
 }
