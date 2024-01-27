4591d810605a6ef535980e11bd49f3c1da27715b
Abhishek <abhishek.one@gmail.com>
2024-01-27 23:18:13 +0530

    Basic product completed

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Login'
 import Register from './Register'
 import Products from './Products'
-import DeleteProduct from './Delete Product'
-import UpdateProduct from './Update Product'
 import Protected from './Protected'
 
 function App() {
@@ -16,8 +14,6 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/products" element={<Protected testprop={<Products />} />} />
-        <Route path="/delete" element={<Protected testprop={<DeleteProduct />} /> } />
-        <Route path="/update" element={<Protected testprop={<UpdateProduct />} />} />
         <Route path="*" element={<Login />} />
       </Routes>
     </Router>
--- src/Products.js
+++ src/Products.js
@@ -1,7 +1,7 @@
 import { useEffect, useState, useCallback } from 'react';
 import Header from './Header';
-import Popup from './Popup';
 import Button from 'react-bootstrap/Button';
+import Modal from "react-bootstrap/Modal";
 
 function Products() {
   const [modalShow, setModalShow] = useState(false);
@@ -9,12 +9,145 @@ function Products() {
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const [errors, setErrors] = useState({});
+  const [mode, setMode] = useState('add');
+  const [productId, setProductId] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
 
-  const fetchProducts = useCallback(async () => {
+  useEffect(() => {
+    setName("");
+    setImage("");
+    setErrors({});
+  }, [modalShow]);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    if (name === "name") {
+      setName(value);
+    } else if (name === "image") {
+      setImage(e.target.files[0]);
+    }
+  };
+
+  const validateForm = () => {
+    let newErrors = {};
+
+    if (!name.trim()) {
+      newErrors.name = "Name is required";
+    } else {
+      newErrors.name = "";
+    }
+
+    setErrors(newErrors);
+
+    return Object.keys(newErrors).every((key) => newErrors[key] === "");
+  };
+
+  async function AddProduct(e){
+    e.preventDefault();
+
+    if (validateForm()) {
+      const formData = new FormData();
+      formData.append("name", name);
+      formData.append("image", image);
+
+      try {
+        const result = await fetch("http://127.0.0.1:8000/api/create-product", {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+          },
+          mode: "cors",
+          body: formData,
+        });
+
+        const resultData = await result.json();
+
+        if (resultData.status === 1) {
+          setModalShow(false);
+          fetchProducts(searchQuery);
+        } else {
+          console.log(resultData);
+        }
+      } catch (error) {
+        console.error("Error occurred:", error);
+      }
+    } else {
+      console.log("Form validation failed");
+    }
+  };
+
+  async function EditProduct(e, id){
+    e.preventDefault();
+
+    if (validateForm()) {
+      const formData = new FormData();
+      formData.append("name", name);
+      formData.append("image", image);
+
+      try {
+        const result = await fetch(`${'http://127.0.0.1:8000/api/edit-product/'+id}`, {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+          },
+          mode: "cors",
+          body: formData,
+        });
+
+        const resultData = await result.json();
+
+        if (resultData.status === 1) {
+          setModalShow(false);
+          fetchProducts(searchQuery);
+        } else {
+          console.log(resultData);
+        }
+      } catch (error) {
+        console.error("Error occurred:", error);
+      }
+    } else {
+      console.log("Form validation failed");
+    }
+  };
+
+  async function DeleteProduct(e, id){
+    e.preventDefault();
+
+    let data = {id};
+
+    try {
+      const result = await fetch("http://127.0.0.1:8000/api/delete-product", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        mode: "cors",
+        body: JSON.stringify(data),
+      });
+
+      const resultData = await result.json();
+
+      if (resultData.status === 1) {
+        fetchProducts(searchQuery);
+      } else {
+        console.log(resultData);
+      }
+    } catch (error) {
+      console.error("Error occurred:", error);
+    }
+  };
+
+  const fetchProducts = useCallback(async (searchQuery) => {
+
     setLoading(true);
 
     try {
-      const response = await fetch(`http://127.0.0.1:8000/api/view-products?page=${currentPage}`, {
+      const response = await fetch(`http://127.0.0.1:8000/api/view-products?page=${currentPage}&search=${searchQuery}`, {
         method: 'GET',
         headers: {
           Accept: 'application/json',
@@ -27,7 +160,6 @@ function Products() {
       }
 
       const resultData = await response.json();
-      console.log('result', resultData);
 
       if (resultData.data) {
         setProducts(resultData.data.data);
@@ -43,11 +175,15 @@ function Products() {
   }, [currentPage]);
 
   useEffect(() => {
-    fetchProducts();
-  }, [fetchProducts, currentPage]);
+    fetchProducts(searchQuery);
+  }, [fetchProducts, currentPage, searchQuery]);
 
   const handlePageChange = (newPage) => {
     setCurrentPage(newPage);
+  };
+
+  const handleSearch = () => {
+    fetchProducts(searchQuery);
   };
 
   return (
@@ -64,11 +200,96 @@ function Products() {
             + Add Product
           </Button>
 
-          <Popup show={modalShow} onHide={() => setModalShow(false)} />
+          <Modal
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+            show={modalShow}
+            onHide={() => setModalShow(false)}
+          >
+            <form
+              onSubmit={(e) =>
+                mode === "edit" ? EditProduct(e, productId) : AddProduct(e)
+              }
+            >
+              <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                  Add Product Details
+                </Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <div>
+                  <label className="pb-2">Product name</label>
+                  <input
+                    className="form-control"
+                    placeholder="Please enter product name"
+                    name="name"
+                    type="text"
+                    value={name}
+                    onChange={handleInputChange}
+                  />
+                  {errors.name && (
+                    <span
+                      style={{
+                        color: "red",
+                        fontSize: "13px",
+                        paddingLeft: "1%",
+                      }}
+                    >
+                      {errors.name}
+                    </span>
+                  )}
+
+                  <br />
+                  <div className="">
+                    <div>
+                      <label className="pb-2">Upload Logo</label>
+                    </div>
+                    <div>
+                      <input
+                        type="file"
+                        name="image"
+                        onChange={handleInputChange}
+                      />
+                    </div>
+                    {errors.image && (
+                      <span
+                        style={{
+                          color: "red",
+                          fontSize: "13px",
+                          paddingLeft: "1%",
+                        }}
+                      >
+                        {errors.image}
+                      </span>
+                    )}
+                  </div>
+                </div>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button className="btn btn-primary" type="submit">
+                  Add
+                </Button>
+                <Button
+                  className="btn btn-secondary"
+                  onClick={() => setModalShow(false)}
+                >
+                  Close
+                </Button>
+              </Modal.Footer>
+            </form>
+          </Modal>
 
           <div className="d-flex">
-            <input type="text" className="form-control" />
-            <button className="btn btn-secondary btn-sm">Search</button>
+            <input
+              type="text"
+              className="form-control"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <button className="btn btn-secondary btn-sm" onClick={handleSearch}>
+              Search
+            </button>
           </div>
         </div>
         <table className="table table-striped table-bordered">
@@ -88,20 +309,41 @@ function Products() {
                   <td>{product.name}</td>
                   <td>
                     <img
-                      src={`http://127.0.0.1:8000/images/${product.image}`  || 'placeholder-image-url'}
+                      src={
+                        product.image
+                          ? `http://127.0.0.1:8000/images/${product.image}`
+                          : `http://127.0.0.1:3000/images/default.png`
+                      }
                       alt="Product"
-                      style={{ width: '85px'}}
+                      style={{ width: "85px" }}
                     />
                   </td>
                   <td>
-                    <button className="btn btn-info btn-sm m-1">Edit</button>
-                    <button className="btn btn-danger btn-sm m-1">Delete</button>
+                    <button
+                      className="btn btn-info btn-sm m-1"
+                      onClick={(e) => {
+                        setMode("edit");
+                        setModalShow(true);
+                        setProductId(product.id);
+                      }}
+                    >
+                      Edit
+                    </button>
+
+                    <button
+                      className="btn btn-danger btn-sm m-1"
+                      onClick={(e) => DeleteProduct(e, product.id)}
+                    >
+                      Delete
+                    </button>
                   </td>
                 </tr>
               ))
             ) : (
               <tr>
-                <td colSpan="4">{loading ? 'Loading...' : 'No products found.'}</td>
+                <td colSpan="4">
+                  {loading ? "Loading..." : "No products found."}
+                </td>
               </tr>
             )}
           </tbody>
@@ -117,7 +359,9 @@ function Products() {
           {Array.from({ length: totalPages }, (_, index) => (
             <button
               key={index}
-              className={`btn btn-primary btn-sm m-2 ${index + 1 === currentPage ? 'active' : ''}`}
+              className={`btn btn-primary btn-sm m-2 ${
+                index + 1 === currentPage ? "active" : ""
+              }`}
               onClick={() => handlePageChange(index + 1)}
             >
               {index + 1}
